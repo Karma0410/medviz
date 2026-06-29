765ba15cac5eb1c04a5132d5f8545a5d50ad0381
Akram Ridou <akram.ridou@epita.fr>
2026-06-29 22:24:42 +0200

    Resolve "feat: documentation"

---
+++ documentation/docusaurus.config.ts
@@ -0,0 +1,101 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'MedViz',
+  tagline: 'Documentation',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://akram.ridou.gitlab.cri.epita.io',
+  baseUrl: '/medviz/',
+
+  organizationName: 'akram.ridou',
+  projectName: 'medviz',
+
+  onBrokenLinks: 'throw',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          routeBasePath: '/',
+        },
+
+        blog: false,
+
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    image: 'img/docusaurus-social-card.jpg',
+
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+
+    navbar: {
+      title: 'MedViz',
+      logo: {
+        alt: 'MedViz Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Docs',
+        },
+        {
+          href: 'https://gitlab.cri.epita.fr/akram.ridou/medviz',
+          label: 'GitLab',
+          position: 'right',
+        },
+      ],
+    },
+
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Introduction',
+              to: '/intro',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitLab',
+              href: 'https://gitlab.cri.epita.fr/akram.ridou/medviz',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} MedViz`,
+    },
+
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
